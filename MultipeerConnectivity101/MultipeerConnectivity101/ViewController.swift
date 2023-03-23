95b14beff7fccfa00443ba448d4eed9687ad3124
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-03-23 15:58:32 -0400

    Add background and auto layout

---
+++ MultipeerConnectivity101/MultipeerConnectivity101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipeerConnectivity101
+//
+//  Created by Victor Vasconcelos on 23/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
